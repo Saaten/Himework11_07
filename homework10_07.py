75538a42dc045e6ad454e574ad71bd72676a60bf
Saaten <136030812+Saaten@users.noreply.github.com>
2023-07-19 00:58:57 +0400

    homework10_07
    էկրանին տպել ներմուծված թիվը զույգ է թե կենտ

---
+++ homework10_07.py
@@ -0,0 +1,9 @@
+print("Input a number")
+a=int(input())
+
+if a<=0:
+    print("The number is neither odd nor even")
+elif a%2==0:
+    print("It's an even number")
+else:
+    print("It's an odd number")
